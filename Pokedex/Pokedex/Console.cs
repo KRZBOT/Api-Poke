d34960ce59cbe06eaa4aa3bf59a2a31f504db137
Jeremy <jeremy.krzeczowski77@gmail.com>
2020-12-28 18:31:04 +0100

    Ajout des commandes dans Console.cs

--- Pokedex/Pokedex/Console.cs
+++ Pokedex/Pokedex/Console.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Pokedex
 {
@@ -6,10 +7,47 @@ namespace Pokedex
     {
         static void Main(string[] args)
         {
+            Boolean running = true;
+            var commands = new Dictionary<string, Action<string>>
+            {
+                ["liste"] = ListeCommand,
+            };
+
             Console.ForegroundColor = ConsoleColor.Cyan;
             Centrer("Bienvenue sur le Pokedex");
             Centrer("Voici les commandes dont vous aurez besoin pour l'utiliser\n");
-            Help();
+            HelpCommand("");
+
+            do
+            {
+                var arg = Console.ReadLine().Split(' ');
+                if (arg[0].StartsWith("/"))
+                {
+                    if (commands.TryGetValue(arg[0].Substring(1), out var command))
+                    {
+                        command(arg[0]);
+                    }
+                }
+                else
+                {
+                    string commande = String.Concat(arg);
+                    switch (commande)
+                    {
+                        case "help":
+                            HelpCommand("");
+                            break;
+                        case "exit":
+                            running = false;
+                            break;
+                        default :
+                            if(commande != "")
+                            {
+                                Console.WriteLine("Nous n'avons pas compris cette commande. Tapez /help pour plus d'infos");
+                            }
+                            break;
+                    }
+                }
+            } while(running);       
         }
 
         public static void Centrer(string texte)
@@ -19,10 +57,10 @@ namespace Pokedex
             Console.WriteLine(texte);
         }
 
-        public static void Help()
+        public static void HelpCommand(string arg)
         {
             Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.Write("/help");
+            Console.Write("\nhelp");
             Console.ResetColor();
             Console.WriteLine(" : affiche la liste des commandes disponibles et leurs descriptions");
             Console.ForegroundColor = ConsoleColor.Magenta;
@@ -36,7 +74,43 @@ namespace Pokedex
             Console.ForegroundColor = ConsoleColor.Magenta;
             Console.Write("/pokemon name");
             Console.ResetColor();
-            Console.WriteLine(" : affiche les détails du pokemon dont le nom est name (nom anglais seulement) dans le pokedex. (Exemple : /pokemon pikachu)");
+            Console.WriteLine(" : affiche les détails du pokemon dont le nom anglais est 'name' dans le pokedex. (Exemple : /pokemon charizard)");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("exit");
+            Console.ResetColor();
+            Console.WriteLine(" : Quitter le pokédex");
+        }
+
+        public static void ListeCommand(string arg)
+        {
+            Console.WriteLine("Voici la liste des pokemons : ");
+            //Inserer requete api ici
+            //
+            //
+            //
+            /*-----------------------*/
+            Console.WriteLine("Appuyez sur espace pour passer à la page suivante et sur echap pour quitter");
+            bool running = true;
+            do
+            {
+                switch (Console.ReadKey().Key)
+                {
+                    case (ConsoleKey.Spacebar):
+                        Console.WriteLine("page suivante..");
+                        // page suivante
+                        break;
+                    case (ConsoleKey.Escape):
+                        Console.WriteLine("_Fermeture de la liste.");
+                        running = false;
+                        break;
+                    default:
+                        break;
+                }
+            } while (running);
+        }
+
+        public static void PokemonCommand(string arg)
+        {
 
         }
     }
